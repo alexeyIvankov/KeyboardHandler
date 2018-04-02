92c3f06dd59ae9813bde06411ae694ddaffc9381
Alexey Ivankov <alexey.ivankov@biz-tek.ru>
2018-04-02 13:21:13 +0300

    - added animation handlers

--- KeyboardHandler/KeyboardHandler.swift
+++ KeyboardHandler/KeyboardHandler.swift
@@ -1,6 +1,4 @@
-//
-//  Лу.swift
-//  OEK
+
 //
 //  Created by Alexey Ivankov on 15.07.16.
 //  Copyright © 2016 Ivankov Alexey. All rights reserved.
@@ -10,13 +8,25 @@ import Foundation
 import UIKit
 
 open class KeyboardHandler {
- 
-    //MARK: Handlers
-    private var willShowHandler:((_ keyboardFrame:CGRect?, _ animationDuration:Double?, _ animationCurve:UIViewAnimationCurve?)->Void)?;
     
-    private var didShowHandler:(()->Void)?;
-    private var willHideHandler:((_ animationDuration:Double?, _ animationCurve:UIViewAnimationCurve?)->Void)?;
-    private var didHideHandler:(()->Void)?;
+    //MARK: Handlers
+    
+    private var willShowHandler:((
+    _ keyboardFrame:CGRect?,
+    _ animationDuration:Double?,
+    _ animationCurve:UIViewAnimationOptions?)->Void)?
+    
+    private var animateWillShowHandler:((
+    _ keyboardFrame:CGRect?)->Void)?
+    
+    private var willHideHandler:((
+    _ animationDuration:Double?,
+    _ animationCurve:UIViewAnimationOptions?)->Void)?
+
+    private var animateWillHideHandler:(()->Void)?
+    
+    private var didShowHandler:(()->Void)?
+    private var didHideHandler:(()->Void)?
     
     //MARK: Notifications
     private var willShowNotification:NSObjectProtocol?;
@@ -24,19 +34,17 @@ open class KeyboardHandler {
     private var willHideNotification:NSObjectProtocol?;
     private var didHideNotification:NSObjectProtocol?;
     
-
-    public init()
-    {
+    
+    public init(){
         subscribeNotifications();
     }
     
-    deinit
-    {
+    deinit{
         unsubscribeNotifications();
         destroyNotifications();
     }
     
-    public func setWillShowHandler(handler:@escaping (_ keyboardFrame:CGRect?, _ animationDuration:Double?, _ animationCurve:UIViewAnimationCurve?)->Void){
+    public func setWillShowHandler(handler:@escaping (_ keyboardFrame:CGRect?, _ animationDuration:Double?, _ animationCurve:UIViewAnimationOptions?)->Void){
         willShowHandler = handler;
     }
     
@@ -44,7 +52,7 @@ open class KeyboardHandler {
         didShowHandler = handler;
     }
     
-    public func setWillHideHandler(handler:@escaping (_ animationDuration:Double?, _ animationCurve:UIViewAnimationCurve?)->Void){
+    public func setWillHideHandler(handler:@escaping (_ animationDuration:Double?, _ animationCurve:UIViewAnimationOptions?)->Void){
         willHideHandler = handler;
     }
     
@@ -52,23 +60,47 @@ open class KeyboardHandler {
         didHideHandler = handler;
     }
     
+    public func setAnimateWillShowHandler(handler:@escaping (_ keyboardFrame:CGRect?)->Void){
+        self.animateWillShowHandler = handler
+    }
+    
+    public func setAnimateWillHideHandler(handler:@escaping()->Void){
+        self.animateWillHideHandler = handler
+    }
+    
     private func subscribeNotifications()
     {
-       willShowNotification = NotificationCenter.default.addObserver(forName: NSNotification.Name.UIKeyboardWillShow, object: nil, queue: OperationQueue.main)  { [weak self]  (notification:Notification)  in
+        willShowNotification = NotificationCenter.default.addObserver(forName: NSNotification.Name.UIKeyboardWillShow, object: nil, queue: OperationQueue.main)  { [weak self]  (notification:Notification)  in
             
             let userInfo = notification.userInfo;
             let keyboardFrame:CGRect? = (userInfo?[UIKeyboardFrameEndUserInfoKey] as AnyObject).cgRectValue;
             let animationDuration:Double? = (userInfo?[UIKeyboardAnimationDurationUserInfoKey] as AnyObject).doubleValue;
-        
-            var animationCurve:UIViewAnimationCurve? = nil
-            let curve = userInfo?[UIKeyboardAnimationCurveUserInfoKey] as? UInt
-        
+            
+            var animationCurve:UIViewAnimationOptions? = nil
+            let curve = userInfo?[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber
+            
             if curve != nil {
-                animationCurve = UIViewAnimationCurve(rawValue: Int(curve! << 16))
+                animationCurve = UIViewAnimationOptions(rawValue: curve!.uintValue)
             }
-        
+            
             if self != nil && self!.willShowHandler != nil{
                 self!.willShowHandler!(keyboardFrame, animationDuration, animationCurve);
+            }
+            
+            
+            if self != nil &&
+                self!.animateWillShowHandler != nil &&
+                animationDuration != nil &&
+                animationCurve != nil{
+                
+                UIView.animate(withDuration: animationDuration!,
+                               delay: 0.0,
+                               options: animationCurve!,
+                               animations:
+                    {
+                        self?.animateWillShowHandler!(keyboardFrame)
+                },
+                               completion: nil)
             }
         }
         
@@ -84,15 +116,31 @@ open class KeyboardHandler {
             let userInfo = notification.userInfo;
             let animationDuration:Double? = (userInfo?[UIKeyboardAnimationDurationUserInfoKey] as AnyObject).doubleValue;
             
-            var animationCurve:UIViewAnimationCurve? = nil
-            let curve = userInfo?[UIKeyboardAnimationCurveUserInfoKey] as? UInt
+            var animationCurve:UIViewAnimationOptions? = nil
+            let curve = userInfo?[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber
             
             if curve != nil {
-                animationCurve = UIViewAnimationCurve(rawValue: Int(curve! << 16))
+                animationCurve = UIViewAnimationOptions(rawValue: curve!.uintValue)
             }
             
             if self != nil && self!.willHideHandler != nil{
-                self!.willHideHandler!(animationDuration, animationCurve);
+                self!.willHideHandler!(animationDuration, nil);
+            }
+            
+            
+            if self != nil &&
+                self!.animateWillHideHandler != nil &&
+                animationDuration != nil &&
+                animationCurve != nil{
+                
+                UIView.animate(withDuration: animationDuration!,
+                               delay: 0.0,
+                               options: animationCurve!,
+                               animations:
+                    {
+                        self?.animateWillHideHandler!()
+                },
+                               completion: nil)
             }
         }
         
@@ -119,5 +167,6 @@ open class KeyboardHandler {
         willHideNotification = nil;
         didHideNotification = nil;
     }
-
+    
 }
+
